2c161a56b96612c4050ff97cf9772c11ce20f46f
prachikrishna <prachikrishna20@gmail.com>
2022-04-12 20:35:30 +0530

    removed

--- contactList_api/entity/contact.go
+++
@@ -1,9 +0,0 @@
-package entity
-
-//Person object for REST(CRUD)
-type Contact struct {
-	ID        int    `json:"id"`
-	FirstName string `json:"firstName"`
-	LastName  string `json:"lastName"`
-	Number    int64  `json:"number"`
-}
