72b5507da32a11974f8684bac01dd7957d8af06a
prachikrishna <prachikrishna20@gmail.com>
2022-05-13 11:24:45 +0530

    adding part 2

--- grpc-server-streaming/grpc/client/client.go
+++ grpc-server-streaming/grpc/client/client.go
@@ -1,18 +1,5 @@
 package main
 
-/*import (
-	"context"
-	"flag"
-	"log"
-	"time"
-
-	//"github.com/golang/protobuf/ptypes"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
-
-	proto "grpc-crud-unary-bookservice/grpc"
-)*/
-
 import (
 	"context"
 	"fmt"
@@ -57,36 +44,6 @@ func printPageItems(client pb.PageInfoServiceClient) {
 	}
 }
 
-/*func main() {
-// get configuration
-address := flag.String("server", "", "gRPC server in format host:port")
-flag.Parse()
-
-// Set up a connection to the server.
-conn, err := grpc.Dial(*address, grpc.WithTransportCredentials(insecure.NewCredentials()))
-if err != nil {
-	log.Fatalf("did not connect: %v", err)
-}
-defer conn.Close()
-
-c := proto.NewBookServiceClient(conn)
-
-ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-defer cancel()
-
-t := time.Now().In(time.UTC)
-//reminder, _ := ptypes.TimestampProto(t)
-pfx := t.Format(time.RFC3339Nano)
-
-req2 := proto.ReadRequest{
-	Id: id,
-}
-res2, err := c.ReadBook(ctx, &req2)
-if err != nil {
-	log.Fatalf("Read failed: %v", err)
-}
-log.Printf("Read result: <%+v>\n\n", res2)*/
-
 func main() {
 	// get configuration
 	address := flag.String("server", "", "gRPC server in format host:port")
@@ -103,20 +60,6 @@ func main() {
 
 	_, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
-
-	//t := time.Now().In(time.UTC)
-	//reminder, _ := ptypes.TimestampProto(t)
-	//pfx := t.Format(time.RFC3339Nano)
-
-	/*serverAddr := net.JoinHostPort(server, serverPort)
-
-	// setup insecure connection
-	conn, err := grpc.Dial(serverAddr, grpc.WithTransportCredentials(insecure.NewCredentials()))
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	//client := pb.NewPageInfoServiceClient(conn)*/
 
 	printPageItems(client)
 }
