77a934202916a0735beca4ae93f8d58cd896b625
prachikrishna <prachikrishna20@gmail.com>
2022-05-16 20:13:22 +0530

    adding gRPC bidirectional service that makes use of oneof in request message

---
+++ grpc-bidirectional-firstPart/go.mod
@@ -0,0 +1,14 @@
+module grpc-bidirectional-firstPart
+
+go 1.17
+
+require (
+	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	golang.org/x/net v0.0.0-20220513224357-95641704303c // indirect
+	golang.org/x/sys v0.0.0-20220513210249-45d2b4557a2a // indirect
+	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/genproto v0.0.0-20220505152158-f39f71e6c8f3 // indirect
+	google.golang.org/grpc v1.46.2 // indirect
+	google.golang.org/protobuf v1.28.0 // indirect
+)
---
+++ grpc-bidirectional-firstPart/matchsvc/matchsvc.go
@@ -0,0 +1,112 @@
+package matchsvc
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"grpc-bidirectional-firstPart/grpc"
+	"io"
+	"log"
+
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+// searchBookServiceServer is implementation of grpc.SearchBookServiceServer proto interface
+type searchBookServiceServer struct {
+	db *sql.DB
+	grpc.UnimplementedSearchBookServiceServer
+}
+
+// NewBookServiceServer creates Book service
+func NewSearchBookServiceServer(db *sql.DB) grpc.SearchBookServiceServer {
+	return &searchBookServiceServer{db: db}
+}
+
+func (s *searchBookServiceServer) connect(ctx context.Context) (*sql.Conn, error) {
+	c, err := s.db.Conn(ctx)
+	if err != nil {
+		return nil, status.Error(codes.Unknown, "failed to connect to database-> "+err.Error())
+	}
+	return c, nil
+}
+
+func (s *searchBookServiceServer) FindMatch(stream grpc.SearchBookService_FindMatchServer) error {
+	ctx := context.Background()
+	c, err := s.connect(ctx)
+	if err != nil {
+		return err
+	}
+	defer c.Close()
+	for {
+		req, err := stream.Recv()
+		if err != nil {
+			if err == io.EOF {
+				return nil // we're done
+			}
+			return err
+		}
+
+		if req.GetName() != "nil" {
+			rows, err := c.QueryContext(ctx, "SELECT `ID`, `Name`, `Author` FROM books WHERE `Name`=? LIMIT 3",
+				req.GetName())
+			if err != nil {
+				return status.Error(codes.Unknown, "failed to select from books-> "+err.Error())
+			}
+			defer rows.Close()
+			if !rows.Next() {
+				if err := rows.Err(); err != nil {
+					return status.Error(codes.Unknown, "failed to retrieve data from books-> "+err.Error())
+				}
+				return status.Error(codes.NotFound, fmt.Sprintf("Book with Name='%s' is not found",
+					req.GetName()))
+			}
+			// get book data
+			var td grpc.Book
+			if err := rows.Scan(&td.Id, &td.Name, &td.Author); err != nil {
+				return status.Error(codes.Unknown, "failed to retrieve field values from books row-> "+err.Error())
+			}
+
+			res := stream.Send(&grpc.Response{
+				Book: &td,
+			})
+
+			if res != nil {
+				log.Fatalf("Error when response was sent to the client: %v", res)
+			}
+		} else if req.GetAuthor() != "nil" {
+			rows, err := c.QueryContext(ctx, "SELECT `ID`, `Name`, `Author` FROM books WHERE `Author`=? LIMIT 3",
+				req.GetAuthor())
+			if err != nil {
+				return status.Error(codes.Unknown, "failed to select from books-> "+err.Error())
+			}
+			defer rows.Close()
+			if !rows.Next() {
+				if err := rows.Err(); err != nil {
+					return status.Error(codes.Unknown, "failed to retrieve data from books-> "+err.Error())
+				}
+				return status.Error(codes.NotFound, fmt.Sprintf("Books with Author='%s' was not found",
+					req.GetName()))
+			}
+			// get book data
+			var td grpc.Book
+			if err := rows.Scan(&td.Id, &td.Name, &td.Author); err != nil {
+				return status.Error(codes.Unknown, "failed to retrieve field values from books row-> "+err.Error())
+			}
+
+			if rows.Next() {
+				return status.Error(codes.Unknown, fmt.Sprintf("found multiple books rows with Author='%s'",
+					req.GetAuthor()))
+			}
+
+			res := stream.Send(&grpc.Response{
+				Book: &td,
+			})
+
+			if res != nil {
+				log.Fatalf("Error when response was sent to the client: %v", res)
+			}
+		}
+
+	}
+}
